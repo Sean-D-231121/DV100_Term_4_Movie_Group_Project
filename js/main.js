b6e8fdb1c9d5015141266e50b00a5c29e877fa84
julianvanstaden <125472051+julianvanstaden@users.noreply.github.com>
2023-10-10 13:46:17 +0200

    Done with hover states
    Still need to add a footer

--- js/main.js
+++ js/main.js
@@ -33,23 +33,4 @@ $(".Heading").hover( function () {
 }, function (){
     $(this).text("POPULAR PICKS");
 });
-}
 
-const particlesConfig = {
-  particles: {
-      number: {
-          value: 50, // Number of particles
-      },
-      size: {
-          value: 3, // Size of particles
-      },
-      color: {
-          value: "#ffffff", // Color of particles
-      },
-      move: {
-          speed: 2, // Speed of particles' movement
-      },
-  },
-};
-
-particlesJS("footer", particlesConfig);
