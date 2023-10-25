41e56d12f99cd8ce4cc5cf1b90bd93a43b06bc8d
Sean-D-231121 <231121@virtualwindow.co.za>
2023-10-25 22:09:47 +0200

    Added more API information

--- js/main.js
+++ js/main.js
@@ -1,100 +1,139 @@
 
 
 let userName;
-
+const trailerWatchURL = "https://www.youtube.com/watch?v=";
+const trailerEmbedURL = "https://www.youtube.com/embed/";
 const movieInformation = [
   {
     imdbId: "tt0083866",
+  moviedbId: "601",
   },
   {
     imdbId: "tt0082971",
+    moviedbId: "85"
   },
   {
     imdbId: "tt0080684",
+    moviedbId: "1891"
   },
   {
     imdbId: "tt0087538",
+    moviedbId:  "1885"
   },
   {
     imdbId: "tt0096895",
+    moviedbId: "268"
   },
   {  
     imdbId: "tt0068646",
+    moviedbId: "238"
   },
   {  
     imdbId: "tt0137523",
+    moviedbId: "550"
   },
   {  
-  imdbId: "tt0114369",
+    imdbId: "tt0114369",
+    moviedbId: "807"
   },
   {  
     imdbId: "tt0816692",
+    moviedbId: "157336"
   },
   {  
     imdbId: "tt0120689",
+    moviedbId: "497"
   },
   {  
     imdbId: "tt0088763",
+    moviedbId: "105"
   },
   {  
     imdbId: "tt0110357",
+    moviedbId: "105"
   },
   {  
     imdbId: "tt15398776",
+    moviedbId: "872585"
   },
   {  
     imdbId: "tt0078748",
+    moviedbId:  "348"
   },
   {  
     imdbId: "tt0910970",
+    moviedbId: "10681"
   },
   {  
     imdbId: "tt0045152",
+    moviedbId: "872"
   },
   {  
     imdbId: "tt0056592",
+    moviedbId: "595"
   },
   {  
     imdbId: "tt1745960",
+    moviedbId: "361743"
   },
   {  
     imdbId: "tt0266697",
+    moviedbId: "24"
   },
   {  
     imdbId: "tt1131734",
+    moviedbId: "19994"
   },
   {  
     imdbId: "tt0266543",
+    moviedbId: "12"
   },
   {  
     imdbId: "tt5726616",
+    moviedbId: "398818"
   },
   {  
     imdbId: "tt1049413",
+    moviedbId: "14160"
   },
   {  
     imdbId: "tt3281548",
+    moviedbId: "331482"
   },
   {  
     imdbId: "tt4925292",
+    moviedbId: "391713"
   },
   {  
     imdbId: "tt13274016",
+    moviedbId: "866463"
   },
   {  
     imdbId: "tt3758542",
+    moviedbId: "365620"
   },
   {  
     imdbId: "tt0110912",
+    moviedbId: "680"
   },
 
 ];
 let showMovieInfo = [];
+let getOMDBInfo = [];
+let getTMDBInfo = [];
 let sortMovies = [];
 let getFilter = "allMovies";
-
+let moviePoster 
+let movieTitle 
+let movieGenre 
+let imdbRating
 $(document).ready(function () {
   getMovieInfo();
+  getTMDBMovieInfo()
+   setTimeout(function () {
+     joinAPIData();
+   }, 300); 
+  
   $(window).on("load", function () {
     userName = JSON.parse(localStorage.getItem("Username"));
     showUserName();
@@ -108,6 +147,7 @@ $(document).ajaxComplete(function () {
       "Individual-movie-page.html?id=" +
       $(this).closest("#movie-card-id").attr("value");
   });
+
 });
 // Get data from OMDB api
 getMovieInfo = () => {
@@ -123,18 +163,61 @@ getMovieInfo = () => {
       moviePoster = movie.Poster;
       movieTitle = movie.Title;
       movieGenre = movie.Genre; // Get the genre information
-
+      imdbRating = movie.imdbRating;
+      boxOffice = movie.BoxOffice;
       showMovieInfo.push({
-        movieId: movieInfo,
+        movieOMDBId: movieInformation[i].imdbId,
+        movieTMDBId: movieInformation[i].moviedbId,
         showMovieTitle: movieTitle,
         showMovieImage: moviePoster,
         showMovieGenre: movieGenre, // Store the genre information
+        showImdbRating: +imdbRating,
+        showBoxOffice: boxOffice,
+        showYear: movie.Year,
+        plot: movie.Plot,
+        runtime: movie.Runtime,
+        showTrailerEmbed: "",
+        showTrailerWatch: "",
       });
+     
+    });
+     
+     
+  }
+  
+};
+// get The movie database data
+getTMDBMovieInfo = () => {
+  for (let i = 0; i < movieInformation.length; i++) {
+    $.ajax({
+      type: "GET",
+      url:
+        "https://api.themoviedb.org/3/movie/" +
+        movieInformation[i].moviedbId +
+        "?api_key=b73d45f4cde3956c846eaca8f95d17d8&append_to_response=videos,images",
+      success: function (data) {
+        movieDBLog = data;
+      },
+    }).done(function () {
+      getTMDBInfo.push({
+      showTrailerEmbed : trailerEmbedURL + movieDBLog.videos.results[0].key,
+      showTrailerWatch : trailerWatchURL + movieDBLog.videos.results[0].key,
+      })
+       
     });
   }
-  console.log(showMovieInfo);
-};
-
+  
+}
+// Joins the data between OMDB and TMDB
+joinAPIData = () =>{
+    for (let i = 0; i < movieInformation.length; i++) {
+      const movie = showMovieInfo[i];
+      movie.showTrailerEmbed = getTMDBInfo[i].showTrailerEmbed;
+      movie.showTrailerWatch = getTMDBInfo[i].showTrailerWatch;
+    }
+  
+  console.log(showMovieInfo)
+}
 // Make cards for movies
 setMovieData = (displayCards) => {
   $("#library-cards-container").empty();
@@ -144,7 +227,7 @@ setMovieData = (displayCards) => {
     let currentCard = $("#library-cards-container").children().eq(i);
     currentCard.find("#movieTitle").text(displayCards[i].showMovieTitle);
     currentCard.find("#movie-image").attr("src", displayCards[i].showMovieImage);
-    currentCard.find("#movie-card-id").attr("value", displayCards[i].movieId);
+    currentCard.find("#movie-card-id").attr("value", displayCards[i].movieTMDBId);
     currentCard.find("#genreText").text(displayCards[i].showMovieGenre); // Populate the genre
   }
 };
